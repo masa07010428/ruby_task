e1378bf15452d36841d5b17345df44719d6f69a6
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-01-17 23:05:03 +0900

    Q1~Q5”

--- task.rb
+++ task.rb
@@ -5,7 +5,10 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names << "斎藤"
+  names.each do |name|
+    puts name
+  end
 end
 
 def q2
@@ -13,21 +16,22 @@ def q2
   array2 = %w(bird bat tiger)
 
   # 以下に回答を記載
-
+  p array1 + array2
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-
+  puts numbers.count(3)
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-
+  sports.delete(nil)
+  p sports
 end
 
 def q5
@@ -35,7 +39,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
