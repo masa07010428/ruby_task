48b3578a4bdcc2765243dc8ed7f54996d8833c9e
Masaru Kubo <mkmkmk04281209@gmail.com>
2020-01-18 18:02:41 +0900

    modified Q10,13,15 and add Q16

--- task.rb
+++ task.rb
@@ -85,13 +85,20 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-  foods.each {|food|
+  # foods.each {|food|
+  #   if food.include?("うに")
+  #     puts "#{food}は、好物です"
+  #   else
+  #     puts "#{food}は、まぁまぁ好きです"
+  #   end
+  # }
+  foods.each do |food|
     if food.include?("うに")
       puts "#{food}は、好物です"
     else
       puts "#{food}は、まぁまぁ好きです"
     end
-  }
+  end
 end
 
 
@@ -119,8 +126,10 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-  user_data[:age] = 32
-  user_data[:address] = "沖縄"
+  # user_data[:age] = 32
+  # user_data[:address] = "沖縄"
+  # puts user_data
+  user_data = user_data.merge(update_data)
   puts user_data
 end
 
@@ -136,17 +145,19 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.keys.include?(:age)
-    puts "OK"
-  else
-    puts "NG"  
-  end
+  # if data1.keys.include?(:age)
+  #   puts "OK"
+  # else
+  #   puts "NG"  
+  # end
 
-  if data2.keys.include?(:age)
-    puts "OK"
-  else
-    puts "NG"  
-  end
+  # if data2.keys.include?(:age)
+  #   puts "OK"
+  # else
+  #   puts "NG"  
+  # end
+  puts data1.keys.include?(:age) ? "OK" : "NG"
+  puts data2.keys.include?(:age) ? "OK" : "NG"
 end
 
 def q16
@@ -158,7 +169,9 @@ def q16
   ]
 
   # 以下に回答を記載
-
+  users.each do |user|
+    puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}です。"
+  end
 end
 
 class UserQ17
